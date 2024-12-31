4802ef092418a0ad56db08d801d4b26be44cdffd
Pradikshan <pradikshan234@gmail.com>
2024-12-31 20:42:24 +0530

    Added logout button

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -12,14 +12,25 @@ const UserProfile = () => {
         <p>User name</p>
         <img
           src="../images/icons/dropdown.png"
-          alt="dropdown"
-          className={`${
+          alt="dropdown-icon"
+          className={`ms-2 ${
             isOpen
               ? "userprofile-container-img-active"
               : "userprofile-container-img"
           }`}
         />
       </div>
+      <button className="logout-btn">
+        <img src="../images/icons/logout.png" alt="logout-icon" />
+        <p className="ms-2">Logout</p>
+      </button>
+      {/* <div className="flex items-center mt-4 border-t-2 border-slate-400">
+        <img
+          src="../images/icons/logout.png"
+          alt="logout-icon"
+          className="mt-3"
+        />
+      </div> */}
     </div>
   );
 };
