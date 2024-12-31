30e70a0b1d99c773b97ef24617585322a53404fa
Pradikshan <pradikshan234@gmail.com>
2024-12-31 20:28:48 +0530

    Changed flex style

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UserProfile = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const onClick = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <div className="userprofile-container">
-      <p>User name</p>
-      <img src="../images/icons/dropdown.png" alt="dropdown" />
+    <div className="userprofile-container" onClick={onClick}>
+      <div className="flex items-center">
+        <p>User name</p>
+        <img
+          src="../images/icons/dropdown.png"
+          alt="dropdown"
+          className={`${
+            isOpen
+              ? "userprofile-container-img-active"
+              : "userprofile-container-img"
+          }`}
+        />
+      </div>
     </div>
   );
 };
