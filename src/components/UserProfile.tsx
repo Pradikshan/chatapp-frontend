5ae92fb4928d0fc7dd1fafcf86f76083813283e8
Pradikshan <pradikshan234@gmail.com>
2024-12-31 20:18:57 +0530

    Added layout

---
+++ src/components/UserProfile.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserProfile = () => {
+  return (
+    <div className="userprofile-container">
+      <p>User name</p>
+      <img src="../images/icons/dropdown.png" alt="dropdown" />
+    </div>
+  );
+};
+
+export default UserProfile;
