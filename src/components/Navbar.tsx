6c16a8b5bb8c0aa1b06f2b559ea135643ca87965
Pradikshan <pradikshan234@gmail.com>
2024-12-31 20:18:50 +0530

    Added layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import UserProfile from "./UserProfile";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="flex justify-end">
+        <UserProfile />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
