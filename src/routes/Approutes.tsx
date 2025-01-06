ef4387079818403f7c917ec8c7fed2d731610abf
Pradikshan <pradikshan234@gmail.com>
2025-01-06 20:09:00 +0530

    Added login route

--- src/routes/Approutes.tsx
+++ src/routes/Approutes.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
 
 const Approutes = () => {
-  return <div>Approutes</div>;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 };
 
 export default Approutes;
