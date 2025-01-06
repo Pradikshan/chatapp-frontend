400165ae3c9a265da2c0545173a4812ccf603d5e
Pradikshan <pradikshan234@gmail.com>
2025-01-06 20:29:30 +0530

    Changed login page path

--- src/routes/Approutes.tsx
+++ src/routes/Approutes.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Login from "../pages/Login";
+import LoginPage from "../pages/LoginPage";
 
 const Approutes = () => {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<LoginPage />} />
     </Routes>
   );
 };
