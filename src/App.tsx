bd6c91fdcd747254433fe77912cfe2278d31d260
Pradikshan <pradikshan234@gmail.com>
2025-01-06 20:29:17 +0530

    Removed navbar and added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Approutes from "./routes/Approutes";
 
 function App() {
   return (
-    <>
-      <Navbar />
-    </>
+    <Router>
+      <>
+        <Approutes />
+      </>
+    </Router>
   );
 }
 
