28910623137aad852b4538f9ab3d711ce5395db9
Pradikshan <pradikshan234@gmail.com>
2025-01-06 20:29:37 +0530

    Created layout

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className="min-h-screen h-screen flex justify-center items-center">
+      <div className="p-5 border-2 border-slate-500 rounded-2xl shadow-xl">
+        <p className="text-center font-bold text-xl">Enter your credentials</p>
+        <form className="mt-6">
+          <div className="my-3">
+            <label htmlFor="user-name" className="font-bold text-lg">
+              User Name:
+            </label>
+            <input
+              type="text"
+              id="user-name"
+              className="border-2 border-black rounded-xl mx-2"
+            />
+          </div>
+
+          <div className="my-3">
+            <label htmlFor="password" className="font-bold text-lg">
+              Password:
+            </label>
+            <input
+              type="text"
+              id="password"
+              className="border-2 border-black rounded-xl mx-2"
+            />
+          </div>
+
+          <button className="login-btn">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
