012f82250e6b7721ed1caed3221ff7682b0a0e65
Pradikshan <pradikshan234@gmail.com>
2025-01-07 20:53:57 +0530

    Imported split layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import SplitLayout from "../components/SplitLayout";
+import ActiveUsers from "../components/ActiveUsers";
 
 const HomePage = () => {
   return (
     <>
       <Navbar />
+      <SplitLayout>
+        <ActiveUsers />
+      </SplitLayout>
     </>
   );
 };
