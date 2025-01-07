a53058a41c9b23887499fdfcd3017e206b356946
Pradikshan <pradikshan234@gmail.com>
2025-01-07 22:38:39 +0530

    Added chat elements

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import ChatHeader from "./ChatHeader";
+import ChatBody from "./ChatBody";
+import ChatInput from "./ChatInput";
 
 const ChatContainer = () => {
-  return <div>ChatContainer</div>;
+  return (
+    <div className="border-2 border-black w-full">
+      <ChatHeader />
+      <ChatBody />
+      <ChatInput />
+    </div>
+  );
 };
 
 export default ChatContainer;
