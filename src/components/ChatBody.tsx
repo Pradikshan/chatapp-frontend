e2c14b6dde5ab913f1d5458ba764b7feb3658d9d
Pradikshan <pradikshan234@gmail.com>
2025-01-08 21:28:22 +0530

    Added mock elements

--- src/components/ChatBody.tsx
+++ src/components/ChatBody.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const ChatBody = () => {
-  return <div>ChatBody</div>;
+  return (
+    <div className="flex flex-col border-2 border-red-500 h-full p-3">
+      <div className="place-self-start p-5 rounded-3xl bg-amber-400">
+        <p>Hello there!</p>
+      </div>
+      <div className="place-self-end p-5 rounded-3xl bg-green-400">
+        <p>General Kenobi!</p>
+      </div>
+    </div>
+  );
 };
 
 export default ChatBody;
