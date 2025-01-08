25d1375aa8fcbe5ef891b1bc0e5cb0ba1aa86baf
Pradikshan <pradikshan234@gmail.com>
2025-01-08 21:28:37 +0530

    Added elements

--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const ChatHeader = () => {
-  return <div>ChatHeader</div>;
+  return (
+    <div className="chat-header-container">
+      <p className="text-xl font-bold">Group Chat</p>
+      {/* <button></button> */}
+    </div>
+  );
 };
 
 export default ChatHeader;
