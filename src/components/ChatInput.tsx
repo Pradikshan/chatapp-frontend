bb705f3b94fb1bc0c00aa4d52fbcb7e09fdb9e4e
Pradikshan <pradikshan234@gmail.com>
2025-01-08 21:28:43 +0530

    Added layout

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const ChatInput = () => {
-  return <div>ChatInput</div>;
+  return (
+    <div className="chat-input-container">
+      <div className="flex justify-between">
+        <input
+          type="text"
+          id="message"
+          className="border-2 border-black rounded-3xl w-full p-3"
+          placeholder="Type your message here..."
+        />
+        <button className="message-send-btn">Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default ChatInput;
