3722bface8af6ea4d6abcb6d9b128bc9b2c3d2d9
Pradikshan <pradikshan234@gmail.com>
2025-01-12 22:26:17 +0530

    Connected to socketIO

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Approutes from "./routes/Approutes";
 
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:4000/");
+
 function App() {
   return (
     <Router>
