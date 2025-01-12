6aa1f96c1624b2aadfb2a2720ef595375d0dd1ae
Pradikshan <pradikshan234@gmail.com>
2025-01-12 22:44:01 +0530

    Changed backend port

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Approutes from "./routes/Approutes";
 
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000/");
+const socket = io("http://localhost:3000/");
 
 function App() {
   return (
