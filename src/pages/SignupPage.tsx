e7197852316df0559b47b329b54ad3f38c953d7d
Pradikshan <pradikshan234@gmail.com>
2025-01-15 18:05:39 +0530

    Created page

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import UserForm from "../components/UserForm";
 
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  const handleSignUp = () => {};
+  return (
+    <div className="min-h-screen h-screen flex justify-center items-center">
+      <div className="p-5 border-2 border-slate-500 rounded-2xl shadow-xl">
+        <p className="text-center font-bold text-xl">Enter your credentials</p>
+
+        <UserForm buttonText={"Signup"} buttonFunc={handleSignUp} />
+
+        <a href="/login" className="form-link">
+          Already have an account? Log in
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default SignupPage;
