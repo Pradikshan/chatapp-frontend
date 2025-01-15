454740abb7b868b96d6b6407aaeddadf533ead34
Pradikshan <pradikshan234@gmail.com>
2025-01-15 18:05:24 +0530

    Created user form component

---
+++ src/components/UserForm.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const UserForm = ({
+  buttonText,
+  buttonFunc,
+}: {
+  buttonText: string;
+  buttonFunc: React.MouseEventHandler<HTMLButtonElement>;
+}) => {
+  return (
+    <div>
+      <form className="mt-6">
+        <div className="my-3">
+          <label htmlFor="user-name" className="font-bold text-lg">
+            User Name:
+          </label>
+          <input
+            type="text"
+            id="user-name"
+            className="border-2 border-black rounded-xl mx-2"
+          />
+        </div>
+
+        <div className="my-3">
+          <label htmlFor="password" className="font-bold text-lg">
+            Password:
+          </label>
+          <input
+            type="text"
+            id="password"
+            className="border-2 border-black rounded-xl mx-2"
+          />
+        </div>
+
+        <button id="form-btn" className="form-btn" onClick={buttonFunc}>
+          {buttonText}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default UserForm;
