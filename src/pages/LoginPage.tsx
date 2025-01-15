eaaf8efe087294f532ff422d1d88d7985d1c2de4
Pradikshan <pradikshan234@gmail.com>
2025-01-15 18:02:30 +0530

    Refactored form into a component

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import UserForm from "../components/UserForm";
 
 const LoginPage = () => {
   const navigate = useNavigate();
@@ -12,7 +13,7 @@ const LoginPage = () => {
     <div className="min-h-screen h-screen flex justify-center items-center">
       <div className="p-5 border-2 border-slate-500 rounded-2xl shadow-xl">
         <p className="text-center font-bold text-xl">Enter your credentials</p>
-        <form className="mt-6">
+        {/* <form className="mt-6">
           <div className="my-3">
             <label htmlFor="user-name" className="font-bold text-lg">
               User Name:
@@ -38,7 +39,8 @@ const LoginPage = () => {
           <button id="login-btn" className="login-btn" onClick={handleLogin}>
             Login
           </button>
-        </form>
+        </form> */}
+        <UserForm buttonText={"Login"} buttonFunc={handleLogin} />
 
         <a href="/signup" className="signup-link">
           Sign up
