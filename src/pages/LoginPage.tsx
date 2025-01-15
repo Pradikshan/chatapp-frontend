f22b8338407bcdb09a7ea1d9becd0a79a60e7158
Pradikshan <pradikshan234@gmail.com>
2025-01-15 20:37:35 +0530

    Added code to handle login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { UserFormData } from "../utils/type";
 
 const LoginPage = () => {
+  const [storedUserData, setStoredUserData] = useState<UserFormData>({
+    name: "",
+    password: "",
+  });
+
+  const [currentFormData, setCurrentFormData] = useState<UserFormData>({
+    name: "",
+    password: "",
+  });
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setCurrentFormData({
+      ...currentFormData,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("userData");
+
+    if (storedData) {
+      const userData: UserFormData = JSON.parse(storedData);
+      setStoredUserData(userData);
+    }
+  }, []);
+
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    navigate("/home");
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (
+      currentFormData.name == storedUserData.name &&
+      currentFormData.password == storedUserData.password
+    ) {
+      navigate("/home");
+    } else {
+      alert("Invalid credentails");
+    }
   };
 
   return (
     <div className="min-h-screen h-screen flex justify-center items-center">
       <div className="p-5 border-2 border-slate-500 rounded-2xl shadow-xl">
         <p className="text-center font-bold text-xl">Enter your credentials</p>
-        <form className="mt-6">
+        <form className="mt-6" onSubmit={handleLogin}>
           <div className="my-3">
-            <label htmlFor="user-name" className="font-bold text-lg">
+            <label htmlFor="name" className="font-bold text-lg">
               User Name:
             </label>
             <input
               type="text"
-              id="user-name"
+              id="name"
+              name="name"
               className="border-2 border-black rounded-xl mx-2"
+              onChange={handleChange}
             />
           </div>
 
@@ -29,18 +66,15 @@ const LoginPage = () => {
               Password:
             </label>
             <input
-              type="text"
+              type="password"
               id="password"
+              name="password"
               className="border-2 border-black rounded-xl mx-2"
+              onChange={handleChange}
             />
           </div>
 
-          <button
-            id="form-btn"
-            className="form-btn"
-            type="submit"
-            onClick={handleLogin}
-          >
+          <button id="form-btn" className="form-btn" type="submit">
             Login
           </button>
         </form>
