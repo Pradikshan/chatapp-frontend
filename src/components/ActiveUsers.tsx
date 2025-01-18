095d6132a362826c5c485da1b8363a86c14f9bd3
Pradikshan <pradikshan234@gmail.com>
2025-01-18 16:40:09 +0530

    Added logic to retrieve active users

--- src/components/ActiveUsers.tsx
+++ src/components/ActiveUsers.tsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useAppStore from "../store/store";
+import { IUser } from "../utils/interface";
 
 const ActiveUsers = () => {
-  const users = ["John", "Jack", "Jane"];
+  const socket = useAppStore((state) => state.socket);
+
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("newUserResponse", (data) => {
+        console.log(`User data from server: ${JSON.stringify(data)}`);
+        setUsers(data);
+      });
+    }
+
+    return () => {
+      socket?.off("newUserResponse");
+    };
+  }, [socket]);
+
+  // console.log(`Users: ${JSON.stringify(users)}`);
+
+  // const users = ["John", "Jack", "Jane"];
   return (
     <div className="active-users-container">
       <p className="font-bold text-2xl">Active users</p>
       <div className="mt-10">
-        {users.map((item) => (
+        {/* {users.map((item) => (
           <p className="text-xl">{item}</p>
+        ))} */}
+        {users.map(({ username, socketId }) => (
+          <p className="text-xl" key={socketId}>
+            {username}
+          </p>
         ))}
       </div>
     </div>
