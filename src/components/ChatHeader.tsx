4c51d9be9a76a45e3ed27c61f15a35186daa9ac9
Pradikshan <pradikshan234@gmail.com>
2025-01-18 16:40:32 +0530

    Removed commented out code

--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -4,7 +4,6 @@ const ChatHeader = () => {
   return (
     <div className="chat-header-container">
       <p className="text-xl font-bold">Group Chat</p>
-      {/* <button></button> */}
     </div>
   );
 };
