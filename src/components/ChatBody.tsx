477fa4b2ac5fdf3f37e84e5cd3dd44bf1923740f
Pradikshan <pradikshan234@gmail.com>
2025-01-18 14:46:48 +0530

    Added logic to retrieve and display messages accordingly

--- src/components/ChatBody.tsx
+++ src/components/ChatBody.tsx
@@ -1,14 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useAppStore from "../store/store";
+import { IMessage } from "../utils/interface";
 
 const ChatBody = () => {
+  const username = useAppStore((state) => state.username);
+
+  const socket = useAppStore((state) => state.socket);
+
+  const [messages, setMessages] = useState<IMessage[]>([]);
+
+  useEffect(() => {
+    if (socket) {
+      socket?.on("messageResponse", (data) => {
+        setMessages((prevMessage) => [
+          ...prevMessage,
+          {
+            text: data.text,
+            username: data.username,
+            id: data.id,
+            socketId: data.socketId,
+          },
+        ]);
+      });
+    }
+
+    // cleanup function to remove the event listener when the component unmounts
+    // does not cause duplicate event listeners
+    return () => {
+      socket?.off("messageResponse");
+    };
+  }, [socket]);
+
   return (
     <div className="flex flex-col border-2 border-red-500 h-full p-3">
-      <div className="place-self-start p-5 rounded-3xl bg-amber-400">
-        <p>Hello there!</p>
-      </div>
-      <div className="place-self-end p-5 rounded-3xl bg-green-400">
-        <p>General Kenobi!</p>
-      </div>
+      {messages.map((message) =>
+        message.username === username ? (
+          <>
+            <div className="place-self-end" key={message.id}>
+              <p>You</p>
+              {/* <p>General Kenobi!</p> */}
+              <div className=" p-5 rounded-3xl bg-green-400">
+                <p>{message.text}</p>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="place-self-start " key={message.id}>
+            <p>{message.username}</p>
+            {/* <p>Hello there!</p> */}
+            <div className="p-5 rounded-3xl bg-amber-400">
+              <p>{message.text}</p>
+            </div>
+          </div>
+        )
+      )}
     </div>
   );
 };
