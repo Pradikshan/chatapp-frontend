f6483a9234b83e1de8db310efee956f6e4f61eb6
Pradikshan <pradikshan234@gmail.com>
2025-01-18 18:16:06 +0530

    Made minor style changes

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -4,7 +4,7 @@ import ChatInput from "./ChatInput";
 
 const ChatContainer = () => {
   return (
-    <div className="border-2 border-black w-full h-full flex flex-col justify-between">
+    <div className="w-full h-full flex flex-col justify-between">
       <ChatHeader />
       <ChatBody />
       <ChatInput />
