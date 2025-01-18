6e27487eeb9e8e6238fb606658adfb70e5f58131
Pradikshan <pradikshan234@gmail.com>
2025-01-18 12:24:24 +0530

    Added form element

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 
 const ChatInput = () => {
+  const handleMessage = () => {};
   return (
     <div className="chat-input-container">
       <div className="flex justify-between">
-        <input
-          type="text"
-          id="message"
-          className="border-2 border-black rounded-3xl w-full p-3"
-          placeholder="Type your message here..."
-        />
-        <button className="message-send-btn">Send</button>
+        <form>
+          <input
+            type="text"
+            id="message"
+            className="border-2 border-black rounded-3xl w-full p-3"
+            placeholder="Type your message here..."
+          />
+          <button className="message-send-btn">Send</button>
+        </form>
       </div>
     </div>
   );
