61331585267482ddf70f9525f030c49342a8669e
Pradikshan <pradikshan234@gmail.com>
2025-01-18 16:41:10 +0530

    Made minor changes

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -15,38 +15,51 @@ const ChatInput = () => {
   const sendMessage = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (socket) {
-      try {
-        socket?.emit("message", {
-          text: message,
-          username: username,
-          id: `11-01-11-${socket.id}`,
-          socketId: socket.id,
-        });
-      } catch (error) {
-        console.log(`Error sending message: ${error}`);
+    if (message !== "") {
+      if (socket) {
+        try {
+          socket?.emit("message", {
+            text: message,
+            username: username,
+            id: `11-01-11-${socket.id}`,
+            socketId: socket.id,
+          });
+        } catch (error) {
+          console.log(`Error sending message: ${error}`);
+        }
+      } else {
+        console.log(`Socket initialization error`);
       }
-    } else {
-      console.log(`Socket initialization error`);
-    }
 
-    setMessage("");
+      setMessage("");
+    } else {
+      alert("Type a message before clicking send!");
+    }
   };
+
+  const handleTyping = () => {
+    socket?.emit("typing", `${username} is typing...`);
+  };
+
+  const handleStopTyping = () => {
+    socket?.emit("stopTyping");
+  };
+
   return (
     <div className="chat-input-container">
-      <div className="flex justify-between">
-        <form onSubmit={sendMessage} className="w-full">
-          <input
-            type="text"
-            id="message"
-            className="border-2 border-black rounded-3xl w-full p-3"
-            placeholder="Type your message here..."
-            value={message}
-            onChange={handleMessage}
-          />
-          <button className="message-send-btn">Send</button>
-        </form>
-      </div>
+      <form onSubmit={sendMessage} className="flex justify-between">
+        <input
+          type="text"
+          id="message"
+          className="border-2 border-black rounded-3xl w-full p-3"
+          placeholder="Type your message here..."
+          value={message}
+          onChange={handleMessage}
+          onKeyDown={handleTyping}
+          onBlur={handleStopTyping}
+        />
+        <button className="message-send-btn">Send</button>
+      </form>
     </div>
   );
 };
