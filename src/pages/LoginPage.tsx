7bb052e7ef4d1a481570664c17291a990305cfa8
Pradikshan <pradikshan234@gmail.com>
2025-01-18 12:02:15 +0530

    Changed label id and name and added zustand functions

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,45 +1,73 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { UserFormData } from "../utils/type";
+import { IUserFormData } from "../utils/type";
+import useAppStore from "../store/store";
 
 const LoginPage = () => {
-  const [storedUserData, setStoredUserData] = useState<UserFormData>({
-    name: "",
-    password: "",
-  });
+  // const [storedUserData, setStoredUserData] = useState<UserFormData>({
+  //   name: "",
+  //   password: "",
+  // });
 
-  const [currentFormData, setCurrentFormData] = useState<UserFormData>({
-    name: "",
-    password: "",
-  });
+  // const [currentFormData, setCurrentFormData] = useState<UserFormData>({
+  //   name: "",
+  //   password: "",
+  // });
 
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setCurrentFormData({
-      ...currentFormData,
-      [e.target.name]: e.target.value,
-    });
-  }
+  // function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+  //   setCurrentFormData({
+  //     ...currentFormData,
+  //     [e.target.name]: e.target.value,
+  //   });
+  // }
 
-  useEffect(() => {
-    const storedData = localStorage.getItem("userData");
+  // useEffect(() => {
+  //   const storedData = localStorage.getItem("userData");
 
-    if (storedData) {
-      const userData: UserFormData = JSON.parse(storedData);
-      setStoredUserData(userData);
-    }
-  }, []);
+  //   if (storedData) {
+  //     const userData: UserFormData = JSON.parse(storedData);
+  //     setStoredUserData(userData);
+  //   }
+  // }, []);
+
+  // const navigate = useNavigate();
+
+  // const handleLogin = (e: React.FormEvent) => {
+  //   e.preventDefault();
+  //   if (
+  //     currentFormData.name == storedUserData.name &&
+  //     currentFormData.password == storedUserData.password
+  //   ) {
+  //     navigate("/home");
+  //   } else {
+  //     alert("Invalid credentails");
+  //   }
+  // };
 
   const navigate = useNavigate();
+  const [formData, setFormData] = useState<IUserFormData>({ username: "" });
+  const setUsername = useAppStore((state) => state.setUsername);
 
-  const handleLogin = (e: React.FormEvent) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    try {
+      const { name, value } = e.target;
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+      console.log(`${formData.username}`);
+    } catch (error) {
+      console.log(`Error in handle change: ${error}`);
+    }
+  };
+
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (
-      currentFormData.name == storedUserData.name &&
-      currentFormData.password == storedUserData.password
-    ) {
+    try {
+      setUsername(formData.username);
       navigate("/home");
-    } else {
-      alert("Invalid credentails");
+    } catch (error) {
+      console.log(`Error in login: ${error}`);
     }
   };
 
@@ -49,19 +77,19 @@ const LoginPage = () => {
         <p className="text-center font-bold text-xl">Enter your credentials</p>
         <form className="mt-6" onSubmit={handleLogin}>
           <div className="my-3">
-            <label htmlFor="name" className="font-bold text-lg">
+            <label htmlFor="username" className="font-bold text-lg">
               User Name:
             </label>
             <input
               type="text"
-              id="name"
-              name="name"
+              id="username"
+              name="username"
               className="border-2 border-black rounded-xl mx-2"
               onChange={handleChange}
             />
           </div>
 
-          <div className="my-3">
+          {/* <div className="my-3">
             <label htmlFor="password" className="font-bold text-lg">
               Password:
             </label>
@@ -72,7 +100,7 @@ const LoginPage = () => {
               className="border-2 border-black rounded-xl mx-2"
               onChange={handleChange}
             />
-          </div>
+          </div> */}
 
           <button id="form-btn" className="form-btn" type="submit">
             Login
