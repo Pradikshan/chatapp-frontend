839dece3cf8a4e462d072e6b19b18527830f6cab
Pradikshan <pradikshan234@gmail.com>
2025-01-18 13:01:34 +0530

    Modified store for socket

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,18 @@
+import { Socket } from "socket.io-client";
 import { create } from "zustand";
 
 interface AppState {
   username: string;
   setUsername: (username: string) => void;
+  socket: Socket | null;
+  setSocket: (socket: Socket) => void;
 }
 
 const useAppStore = create<AppState>((set) => ({
   username: "",
   setUsername: (username) => set({ username }),
+  socket: null,
+  setSocket: (socket) => set({ socket }),
 }));
 
 export default useAppStore;
