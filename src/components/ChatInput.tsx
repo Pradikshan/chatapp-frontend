d9ee21a93bbc4d8e8edad6aab0a83a833b3dcd41
Pradikshan <pradikshan234@gmail.com>
2025-01-18 12:37:44 +0530

    Added logic to handle messages

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ChatInput = () => {
-  const handleMessage = () => {};
+  const [message, setMessage] = useState<string>("");
+
+  const handleMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value);
+  };
+
+  const sendMessage = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setMessage("");
+    console.log(`The message is: ${message}`);
+  };
   return (
     <div className="chat-input-container">
       <div className="flex justify-between">
-        <form>
+        <form onSubmit={sendMessage} className="w-full">
           <input
             type="text"
             id="message"
             className="border-2 border-black rounded-3xl w-full p-3"
             placeholder="Type your message here..."
+            value={message}
+            onChange={handleMessage}
           />
-          <button className="message-send-btn">Send</button>
         </form>
+        <button className="message-send-btn">Send</button>
       </div>
     </div>
   );
