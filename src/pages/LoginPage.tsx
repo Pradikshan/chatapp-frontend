fd0ed7b5b625199cbc0e4ff3a75bb83ca4c24640
Pradikshan <pradikshan234@gmail.com>
2025-01-24 10:43:01 +0530

    Added modal component

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserFormData } from "../utils/type";
 import useAppStore from "../store/store";
+import LoginModal from "../components/LoginModal";
 
 const LoginPage = () => {
   const socket = useAppStore((state) => state.socket);
@@ -39,6 +40,7 @@ const LoginPage = () => {
 
   return (
     <>
+      <LoginModal />
       <div className="min-h-screen h-screen flex justify-center items-center">
         <div className="p-5 border-2 border-slate-500 rounded-2xl shadow-xl">
           <p className="text-center font-bold text-xl">
